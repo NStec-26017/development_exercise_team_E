26672228ac2e325d9a65c98cb49887b63e6a810e
NStec-26017 <nstec26_017@fullness.email>
2026-08-04 15:37:11 +0900

    EntityのEmployee,EmployeeAccountの変更

--- src/main/java/com/example/fullness/stationary/entity/EmployeeAccount.java
+++ src/main/java/com/example/fullness/stationary/entity/EmployeeAccount.java
@@ -1,8 +1,13 @@
 package com.example.fullness.stationary.entity;
 
-/**
- * 社員アカウントテーブルを示すentity
- */
-public class EmployeeAccount {
+import java.io.Serializable;
 
+import lombok.Data;
+
+@Data
+public class EmployeeAccount implements Serializable {
+    private int id;
+    private int employeeId;
+    private String name;
+    private String password;
 }
