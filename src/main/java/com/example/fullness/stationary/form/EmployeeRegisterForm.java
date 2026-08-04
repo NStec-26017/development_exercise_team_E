55287ed3033f3c3edcc6f74cb4ee7d2d4f5bc9ee
NStec-26020 <nstec26_020@fullness.email>
2026-08-04 16:09:40 +0900

    EmployeeRegisterFormの変更

--- src/main/java/com/example/fullness/stationary/form/EmployeeRegisterForm.java
+++ src/main/java/com/example/fullness/stationary/form/EmployeeRegisterForm.java
@@ -1,14 +1,19 @@
 package com.example.fullness.stationary.form;
 
+import lombok.Data;
+
 /**
  * BP003【アカウント登録(入力)】画面から送信される内容を保持するform
  */
-
+@Data
 public class EmployeeRegisterForm {
 
     /** 選択された社員のID */
+    private int employeeId;
 
     /** 入力されたアカウント名 */
+    private String accountName;
 
     /** 入力されたパスワード */
+    private String password;
 }
