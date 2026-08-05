2e8e54d6447a3cbdfd7126009d8b130165611474
NStec-26020 <nstec26_020@fullness.email>
2026-08-05 10:12:31 +0900

    EmployeeRepositoryの内容追加

--- src/main/java/com/example/fullness/stationary/controller/EmployeeRegisterController.java
+++ src/main/java/com/example/fullness/stationary/controller/EmployeeRegisterController.java
@@ -1,9 +1,14 @@
 package com.example.fullness.stationary.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * BP003【担当者アカウント登録】の画面遷移を担当するController。
  * 
  */
+@Controller
+@RequestMapping()
 public class EmployeeRegisterController {
 
 }
