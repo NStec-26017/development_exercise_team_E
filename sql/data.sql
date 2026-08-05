dbda5bfb60bd3f39d16dce68cb39795150c47375
NStec-26012 <nstec26_012@fullness.email>
2026-08-05 15:46:30 +0900

    data sql

---
+++ sql/data.sql
@@ -0,0 +1,15 @@
+<!--departmentテーブル>
+insert into department (id,name)
+values (1004, '商品管理課');
+
+<!--employeeテーブル>
+insert into employee (id, department_id, name, name_kana)
+values (104, 1004, '豆田豆蔵', 'マメタマメゾウ');
+
+<!--departmentテーブル>
+insert into department (id,name)
+values (1005, '商品管理課');
+
+<!--employeeテーブル>
+insert into employee (id, department_id, name, name_kana)
+values (105, 1005, '空田豆雄', 'ソラタマメオ');
