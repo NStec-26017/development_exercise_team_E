fe530119be75984feb14ad3fc8f5fd626c03da18
NStec-26020 <nstec26_020@fullness.email>
2026-08-05 10:25:46 +0900

    MenuControllerの作成

---
+++ src/main/java/com/example/fullness/stationary/controller/MenuController.java
@@ -0,0 +1,23 @@
+package com.example.fullness.stationary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * バックエンドの「メニュー画面」Controller
+ */
+@Controller
+@RequestMapping("/admin")
+public class MenuController {
+
+    /**
+     * メニュー画面の表示
+     * 
+     * @return メニュー画面(menu)
+     */
+    @GetMapping()
+    public String showMenu() {
+        return "menu";
+    }
+}
