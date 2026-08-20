9b9e4c6a7945368c779e7cd25d07af8561600367
NStec-26012 <nstec26_012@fullness.email>
2026-08-21 08:52:39 +0900

    UC014の途中です

--- src/main/java/com/example/fullness/stationary/service/ProductCategoryService.java
+++ src/main/java/com/example/fullness/stationary/service/ProductCategoryService.java
@@ -11,7 +11,7 @@ import com.example.fullness.stationary.repository.ProductCategoryRepository;
 public class ProductCategoryService {
 
     @Autowired
-    private ProductCategoryRepository categoryrepository;
+    private ProductCategoryRepository productcategoryrepository;
 
     /**
      * 新しい商品カテゴリーを登録する業務処理
@@ -21,13 +21,13 @@ public class ProductCategoryService {
 
         // 1. 【業務チェック】すでに同じ名前のカテゴリがないか調べる
         // (※以前作成したexistsByNameのような仕組みをここで使う)
-        if (categoryrepository.existByName(productCategory.getName())) {
+        if (productcategoryrepository.existByName(productCategory.getName())) {
             throw new RuntimeException("このカテゴリー名はすでに登録されています。");
         }
 
         // 2. 【データベース保存】問題なければ、XMLに書いたcreateメソッドを呼び出す
         // (ここで自動的にシーケンスからIDが取られ、INSERTされる)
-        Boolean isSuccess = categoryrepository.create(productCategory);
+        Boolean isSuccess = productcategoryrepository.create(productCategory);
 
         // 3. 【結果の確認】もし保存に失敗していたらエラーを発生させる
         if (!isSuccess) {
