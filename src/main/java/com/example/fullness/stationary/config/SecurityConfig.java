784a3ec54b3ec17a272fe3fb09610a197a606703
NStec-26017 <nstec26_017@fullness.email>
2026-08-20 13:40:46 +0900

    EmployeeAccountRepositoryTestの変更

--- src/main/java/com/example/fullness/stationary/config/SecurityConfig.java
+++ src/main/java/com/example/fullness/stationary/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -60,6 +61,6 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         // TODO 後でBCryptPasswordEncoderに変更する
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
