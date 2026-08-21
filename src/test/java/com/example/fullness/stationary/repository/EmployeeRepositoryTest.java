6168c0c227d76ad8cd6f4f832e2f137c325c3380
NStec-26020 <nstec26_020@fullness.email>
2026-08-21 09:11:14 +0900

    RepositoryTest修正

--- src/test/java/com/example/fullness/stationary/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/example/fullness/stationary/repository/EmployeeRepositoryTest.java
@@ -1,9 +1,8 @@
 package com.example.fullness.stationary.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Comparator;
 import java.util.List;
