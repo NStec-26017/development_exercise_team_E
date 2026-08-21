9142e18854fccbf3de7679ee91614e153b8c56de
NStec-26017 <nstec26_017@fullness.email>
2026-08-21 16:04:06 +0900

    変更

--- src/main/java/com/example/fullness/stationary/service/ProductDeleteService.java
+++ src/main/java/com/example/fullness/stationary/service/ProductDeleteService.java
@@ -1,24 +1,22 @@
-// package com.example.fullness.stationary.service;
+package com.example.fullness.stationary.service;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-// import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.example.fullness.stationary.repository.ProductRepository;
 
-// import com.example.fullness.stationary.repository.ProductRepository;
+@Service
+public class ProductDeleteService {
+    @Autowired
+    ProductRepository productRepository;
 
-// @Service
-// public class ProductDeleteService {
-// @Autowired
-// ProductRepository productRepository;
-
-// @Transactional
-// public void deleteById(int id) {
-// // 1. リポジトリの deleteById を実行し、更新件数を取得
-// int updatedCount = productRepository.deleteById(id);
-
-// // 2. 更新件数が 0 件の場合は、対象データがない（または既削除）ため例外をスロー
-// if (updatedCount == 0) {
-// throw new RuntimeException("指定された商品が見つからないか、既に削除されています。ID: " + id);
-// }
-// }
-// }
+    @Transactional
+    public void deleteById(int id) {
+        // 1. リポジトリの deleteById を実行し、更新件数を取得
+        int updatedCount = productRepository.deleteById(id);
+        // 2. 更新件数が 0 件の場合は、対象データがない（または既削除）ため例外をスロー
+        if (updatedCount == 0) {
+            throw new RuntimeException("指定された商品が見つからないか、既に削除されています。ID: " + id);
+        }
+    }
+}
