891b9c2f7eeae7cf51f09f0ca0827d4952b24299
NStec-26020 <nstec26_020@fullness.email>
2026-08-24 12:41:35 +0900

    formの作成完了

--- src/main/java/com/example/fullness/stationary/form/ProductForm.java
+++ src/main/java/com/example/fullness/stationary/form/ProductForm.java
@@ -2,6 +2,10 @@ package com.example.fullness.stationary.form;
 
 import java.io.Serializable;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -9,10 +13,15 @@ public class ProductForm implements Serializable {
     /** 商品ID */
     private Integer id;
     /** 商品カテゴリID（外部キー） */
+    @NotNull
     private Integer productCategoryId;
     /** 商品名 */
+    @NotBlank
+    @Size(min = 2, max = 20)
     private String name;
     /** 価格 */
+    @NotNull
+    @Max(1000000)
     private Integer price;
     /** 画像URL */
     private String imagePath;
--- src/main/java/com/example/fullness/stationary/form/ProductStockForm.java
+++ src/main/java/com/example/fullness/stationary/form/ProductStockForm.java
@@ -1,5 +1,16 @@
 package com.example.fullness.stationary.form;
 
-public class ProductStockForm {
+import java.io.Serializable;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class ProductStockForm implements Serializable {
+    private Integer productId;
+
+    @NotNull
+    @Max(1000)
+    private Integer quantity;
 }
